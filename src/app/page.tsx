5806570f74cebf507bc43abc6d5bf95ca54c1b11
BIANCA_TAKAMORI <biancahelenatakamoriribeiro@gmail.com>
2025-09-15 13:16:12 -0300

    test api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>oie</div>;
+  const [showLuck, setShowLuck] = useState(false);
+  const [dados, setDados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [erro, setErro] = useState<string | null>(null);
+
+  const handleClick = async () => {
+    setErro(null);
+    try {
+      const res = await fetch(
+        "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=handleQuotehttps://goquotes-api.herokuapp.com/api/v1/random?count=1"
+      );
+      if (!res.ok) {
+        throw new Error("falha");
+      }
+      const json = await res.json();
+      setDados(json);
+      console.log(json);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErro(error.message);
+      } else {
+        setErro(String(error));
+      }
+    }
+  };
+  return (
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        {!showLuck && (
+          <button
+            className="bg-zinc-800 text-white p-4 rounded"
+            onClick={handleClick}
+          >
+            your luck of the day
+          </button>
+        )}
+        {showLuck && <p> sua frase </p>}
+      </div>
+    </>
+  );
 }
